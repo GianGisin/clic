682c7adae8b937fc409720c622c1430c7f44b411
GianGisin <gian.gisin@gmail.com>
2025-07-12 23:02:15 +0200

    counter function

---
+++ src/clic/lib/counter.py
@@ -0,0 +1,18 @@
+import pathlib
+from .settings import params
+from .count_file import count_lines
+
+
+def count(path: pathlib.Path, settings: params) -> int:
+    if path.is_file() and path.suffix in settings.file_extensions:
+        with path.open() as f:
+            return count_lines(f, settings)
+
+    if path.is_dir():
+        sub_total = 0
+        for p in path.iterdir():
+            sub_total += count(p, settings)
+
+        return sub_total
+
+    return 0
