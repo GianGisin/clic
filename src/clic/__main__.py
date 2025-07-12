31b245c934fcc15888838c1f3f43c29bda3ca879
GianGisin <gian.gisin@gmail.com>
2025-07-12 22:50:56 +0200

    use pathlib, accept file suffix from command line

--- src/clic/__main__.py
+++ src/clic/__main__.py
@@ -1,5 +1,4 @@
-import sys, os
-from enum import Enum
+import sys, os, pathlib
 
 from .lib.settings import params
 from .lib.counter import count
@@ -11,13 +10,25 @@ def main() -> None:
         print("Expected at least one argument: file path")
         sys.exit(os.EX_USAGE)
 
-    # TODO: make relative paths possible
-    count_path = args[1]
-    # TODO: how to differentiate between folder search and a single file?
-    # If a file path is passed in, set the file_extension to the extension of the file
-    print(
-        f"Total Lines: {count(count_path, params(comment_symbol="#", file_extensions=(".py",)))}"
-    )
+    count_path = pathlib.Path(args[1]).expanduser()
+    if not count_path.exists():
+        print(f"The given path ('{count_path}') does not exist")
+        sys.exit(os.EX_USAGE)
+
+    suffixes = set()
+
+    if count_path.is_file():
+        suffixes.add(count_path.suffix)
+    elif len(args) < 3:
+        print("Please provide a file extension to search")
+        sys.exit(os.EX_USAGE)
+    else:
+        suffixes.add(args[2])
+
+    settings = params(comment_symbol="#", file_extensions=suffixes)
+    print(settings)
+
+    print(f"Total Lines: {count(count_path, settings)}")
 
 
 if __name__ == "__main__":
