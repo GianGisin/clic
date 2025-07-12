258139dc0706136c403c3307be987053c185578c
GianGisin <gian.gisin@gmail.com>
2025-07-12 22:49:11 +0200

    file extensions is a set / repr function

--- src/clic/lib/settings.py
+++ src/clic/lib/settings.py
@@ -2,7 +2,7 @@ class params:
     def __init__(
         self,
         comment_symbol: str,
-        file_extensions: tuple[str],
+        file_extensions: set[str],
         count_comments: bool = False,
         count_whitespaces: bool = False,
     ) -> None:
@@ -10,3 +10,6 @@ class params:
         self.file_extensions = file_extensions
         self.count_comments = count_comments
         self.count_whitespaces = count_whitespaces
+
+    def __repr__(self) -> str:
+        return str(self.__dict__.items())
